1ca888355509909dbe32d2814c36e3ccd7f5607c
VascoPaivaa47539 <a47539@alunos.ipb.pt>
2022-12-05 15:38:40 +0000

    Crowd Simulation - Parte 1

---
+++ Assets/Scripts/AIcontroller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AIcontroller : MonoBehaviour
+{
+    public GameObject goal;
+    NavMeshAgent agent;
+
+    
+    void Start()
+    {
+        
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(goal.transform.position);
+    }
+}
+
